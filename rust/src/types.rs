1db1bdb6e2e0e96bed966d655de16d5fbcd420f0
KyleMiles <krm504@nyu.edu>
2021-05-19 20:41:01 -0400

    DWARF Import DebugInfo Plugin

--- rust/src/types.rs
+++ rust/src/types.rs
@@ -757,7 +757,7 @@ impl Type {
         }
     }
 
-    pub fn function<'a, S: BnStrCompatible + Copy, T: Into<Conf<&'a Type>>>(
+    pub fn function<'a, S: BnStrCompatible + Clone, T: Into<Conf<&'a Type>>>(
         return_type: T,
         parameters: &[FunctionParameter<S>],
         variable_arguments: bool,
