0b04a8e3acb85ec2a0be347fba0580eb2d9f285f
Jon Palmisciano <jp@jonpalmisc.com>
2021-08-06 10:39:28 -0400

    StackView: Some doc comments, organization, cleanup

--- ui/stackview.h
+++ ui/stackview.h
@@ -10,13 +10,7 @@
 #include "sidebar.h"
 #include "uitypes.h"
 
-enum class Direction {
-    Up,
-    Down,
-    Left,
-    Right
-};
-
+//! Dialog to enable arbitrary stack variable creation.
 class BINARYNINJAUIAPI CreateStackVariableDialog : public QDialog {
     Q_OBJECT
 
@@ -27,6 +21,7 @@ class BINARYNINJAUIAPI CreateStackVariableDialog : public QDialog {
     QLineEdit* m_nameField;
     QComboBox* m_typeDropdown;
 
+    //! Automatically update/sanitize the offset and name fields after input.
     void autoFillFields();
 
 protected:
@@ -37,6 +32,7 @@ public:
         FunctionRef func, int64_t initialOffset = 0);
 };
 
+//! A single line in the stack view.
 class StackViewLine {
 public:
     enum class Type {
@@ -47,46 +43,84 @@ public:
         Fill
     };
 
+    //! Create a new line for a variable.
     static StackViewLine variable(int64_t offset,
         const BinaryNinja::VariableNameAndType& vnat, PlatformRef plat);
+
+    //! Create a new line for a struct or array member.
     static StackViewLine member(int64_t offset,
         const BinaryNinja::VariableNameAndType& vnat, PlatformRef plat);
+
+    //! Create a new line for a struct offset reference.
     static StackViewLine offsetRef(int64_t base, uint64_t offset, size_t size);
+
+    //! Create a new annotation line.
     static StackViewLine annotation(int64_t offset, const std::string& text);
+
+    //! Create a new fill line.
     static StackViewLine fill(int64_t offset, size_t length);
 
+    //! Get this line's type.
     StackViewLine::Type type() const;
 
+    //! Get the stack frame offset for this line.
     int64_t offset() const;
+
+    //! Get the number of bytes this line represents on the stack.
     size_t width() const;
 
-    int64_t relativeOffset() const;
-    void setRelativeOffset(int64_t relativeOffset);
-
+    //! Get the hierarchical level of this line.
     size_t level() const;
+
+    //! Set the hierarchical level for this line.
     void setLevel(size_t level);
 
+    //! Get the type of the data this line represents.
     TypeRef dataType() const;
+
+    //! Set the type of the data this line represents. Does not affect the underlying variable.
     void setDataType(TypeRef vnat);
 
+    //! Get the variable represented by this line.
     BinaryNinja::Variable variable() const;
+
+    //! Set the variable represented by this line.
     void setVariable(const BinaryNinja::Variable& var);
 
+    //! Set a width override for this line.
     void setWidthOverride(size_t width);
+
+    //! Is this line backed by data or is it ephermeral?
     bool isDataBacked() const;
 
+    //! Is the data represented by this line referenced in the current function?
     bool isReferenced() const;
+
+    //! Set whether the data represented by this line is referenced in the current function.
     void setIsReferenced(bool isReferenced);
 
+    //! Is the data represented by this line unused in the current function?
     bool isUnused() const;
+
+    //! Set whether the data represented by this line unused in the current function.
     void setIsUnused(bool isUnused);
 
+    //! Get the content (with no offset) for this line.
     BinaryNinja::DisassemblyTextLine content() const;
+
+    //! Get the content with the leading offset (or padding) for this line.
     BinaryNinja::DisassemblyTextLine contentWithOffset(bool hide = false) const;
+
+    //! Get the width of the total line content in characters.
     size_t contentWidth(bool withOffset = true) const;
 
+    //! Append a single token to this line's content.
     void appendToken(const BinaryNinja::InstructionTextToken& token);
+
+    //! Append multiple tokens to this line's content.
     void appendTokens(const std::vector<BinaryNinja::InstructionTextToken>& tokens);
+
+    //! Indent this line's content.
     void indent(size_t levels = 1);
 
 private:
@@ -94,7 +128,6 @@ private:
 
     StackViewLine::Type m_type;
     int64_t m_offset;
-    int64_t m_relativeOffset;
     size_t m_level;
 
     TypeRef m_dataType;
@@ -107,8 +140,16 @@ private:
     BinaryNinja::DisassemblyTextLine m_content;
 };
 
-class BINARYNINJAUIAPI StackView : public QAbstractScrollArea,
-                                   public View {
+//! Simple direction enum; used for cursor movement functions.
+enum class Direction {
+    Up,
+    Down,
+    Left,
+    Right
+};
+
+//! The stack view!
+class BINARYNINJAUIAPI StackView : public QAbstractScrollArea, public View {
     Q_OBJECT
 
     ViewFrame* m_view;
@@ -121,9 +162,16 @@ class BINARYNINJAUIAPI StackView : public QAbstractScrollArea,
     size_t m_lineIndex;
     size_t m_tokenIndex;
 
+    //! Bind and register all stack view actions.
     void setupActions();
+
+    //! Internal refresh method, rebuilds all lines.
     void rebuildLines();
 
+    //! Refresh the internal highlight token state.
+    void refreshHighlight();
+
+    //! Find the end of a stack void given a start offset.
     int64_t findVoidEnd(int64_t start) const;
 
 protected:
@@ -136,24 +184,40 @@ public:
 
     //! Refresh the stack view's content.
     void refresh();
-    void moveCursorToMouse(QMouseEvent* event, bool isSelecting);
-    void moveCursor(Direction dir);
-    void sanitizeCursor(Direction preference);
-    void refreshHighlight();
 
+    //! Move the cursor to approximate clicked position.
+    void moveCursorToMouse(QMouseEvent* event, bool isSelecting);
+
+    //! Move the cursor via the keyboard.
+    void moveCursor(Direction dir);
+
+    //! Ensure the cursor is not selecting any prohibited tokens.
+    void sanitizeCursor(Direction preference);
+
+    //! Get the selected StackViewLine.
     StackViewLine selectedLine() const;
 
+    //! Rename the variable belonging to the selected line.
     void renameVariable();
+
+    //! Change the type of the variable belonging to the selected line.
     void retypeVariable();
+
+    //! Undefine the variable belonging to the selected line.
     void undefineVariable();
 
+    //! Show the "Create Variable" dialog.
     void showCreateVariableDialog();
 
-    void quickCreateVariableAtCursor(size_t size);
+    //! Create an integer of the given size at the cursor position.
+    void quickCreateIntegerAtCursor(size_t size);
+
+    //! Create an array at the cursor, spanning until the next stack variable.
     void quickCreateArrayAtCursor();
+
+    //! Create a new struct at the cursor, spanning until the next stack variable.
     void quickCreateStructAtCursor();
 
-    // --- View Interface ---
     BinaryViewRef getData() override;
     uint64_t getCurrentOffset() override;
     void setSelectionOffsets(BNAddressRange range) override;
@@ -162,20 +226,19 @@ public:
     void updateFonts() override;
 };
 
+//! Stack view sidebar widget wrapper.
 class BINARYNINJAUIAPI StackViewSidebarWidget : public SidebarWidget {
     Q_OBJECT
 
-    StackView* m_sv;
+    StackView* m_stackView;
     QWidget* m_header;
 
 public:
     StackViewSidebarWidget(ViewFrame* view, BinaryViewRef data);
 
     void refresh();
-
     void focus() override { refresh(); }
-    void notifyFontChanged() override { m_sv->updateFonts(); }
-
+    void notifyFontChanged() override { m_stackView->updateFonts(); }
     QWidget* headerWidget() override { return m_header; }
 };
 
